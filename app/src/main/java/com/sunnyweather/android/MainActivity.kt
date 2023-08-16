c12d3ed66042a5e040b90eb3295d7f17b5d28e22
Calvin <kaathyrodriguez461@gmail.com>
2023-08-16 11:28:24 +0800

    基础版本

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -16,32 +16,7 @@ import com.sunnyweather.android.ui.theme.SunnyWeatherTheme
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SunnyWeatherTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SunnyWeatherTheme {
-        Greeting("Android")
-    }
-}
